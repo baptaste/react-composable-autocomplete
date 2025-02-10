b8d79a8fbf5952e08cffc30314fcd1cf4e2a4582
baptistechp <baptiste.champbenoit@gmail.com>
2025-02-10 08:00:50 +0100

    fix: correct theme-toggler icon and value for system

--- src/website/app/components/theme/theme-toggler.tsx
+++ src/website/app/components/theme/theme-toggler.tsx
@@ -3,16 +3,27 @@ import { MoonIcon, SunIcon } from "lucide-react";
 
 import { Button } from "@/packages/core/ui/button";
 
-import { useTheme } from "./theme.context";
+import { useTheme, type Theme } from "./theme.context";
 
 export function ThemeToggler() {
   const { theme, setTheme } = useTheme();
-  const Icon = theme === "dark" ? SunIcon : MoonIcon;
-  const nextValue = theme === "dark" ? "light" : "dark";
+
+  let NextIcon = theme === "dark" ? SunIcon : MoonIcon;
+  let nextTheme: Theme = theme === "dark" ? "light" : "dark";
+
+  if (theme === "system") {
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+      .matches
+      ? "dark"
+      : "light";
+
+    NextIcon = systemTheme === "dark" ? SunIcon : MoonIcon;
+    nextTheme = systemTheme === "dark" ? "light" : "dark";
+  }
 
   const toggleTheme = useCallback(() => {
-    setTheme(theme === "dark" ? "light" : "dark");
-  }, [theme, setTheme]);
+    setTheme(nextTheme);
+  }, [nextTheme, setTheme]);
 
   return (
     <Button
@@ -20,8 +31,8 @@ export function ThemeToggler() {
       className="group fixed right-6 top-4 z-20 h-7 px-3 text-xs capitalize sm:right-12 xl:right-40"
       onClick={toggleTheme}
     >
-      <Icon className="mr-2 h-4 w-4 text-foreground/60 transition-colors duration-300 group-hover:text-foreground" />
-      {nextValue}
+      <NextIcon className="mr-2 h-4 w-4 text-foreground/60 transition-colors duration-300 group-hover:text-foreground" />
+      {nextTheme}
     </Button>
   );
 }
